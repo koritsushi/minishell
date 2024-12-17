bc62adcf55fb7b4145722de60a5c69b50729fc33
Lim Chee Tian <lchee-ti@u91z08s04.42kl.edu.my>
2024-12-17 16:42:57 +0800

    header file includes

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,15 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*   By: lchee-ti <lchee-ti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:20:44 by mliyuan           #+#    #+#             */
-/*   Updated: 2024/12/17 16:00:58 by mliyuan          ###   ########.fr       */
+/*   Updated: 2024/12/17 16:35:44 by lchee-ti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-#include "../libft/libft.h"
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <dirent.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+
 
 #endif
