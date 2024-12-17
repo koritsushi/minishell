f2a4997fdd3131aa147a8a6938585de44f8559e6
Lim Chee Tian <lchee-ti@u91z08s04.42kl.edu.my>
2024-12-17 16:54:12 +0800

    Added ft_strcmp.c to libft, header file and Makefile modified to make it work.

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -1,29 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lchee-ti <lchee-ti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/17 16:12:37 by mliyuan           #+#    #+#             */
-/*   Updated: 2024/12/17 16:52:17 by lchee-ti         ###   ########.fr       */
+/*   Created: 2024/06/14 16:55:43 by lchee-ti          #+#    #+#             */
+/*   Updated: 2024/06/14 16:58:52 by lchee-ti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "libft.h"
 
-int	main(int argc, char **argv, char **env)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	char	*input;
-	
-	while (1)
+	size_t	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
 	{
-		input = readline("minishell$ ");
-		if (!input)
-			break;
-		if (*input)
-			add_history(input);
-		if (!ft_strcmp(input, exit))
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
 	}
 	return (0);
 }
