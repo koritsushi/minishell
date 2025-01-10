86be194b230e73a24f5509722099cdc130103909
Kodokushi <lyuan.2581@gmail.com>
2025-01-10 22:24:59 +0800

    add errno.h header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,12 +6,16 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:20:44 by mliyuan           #+#    #+#             */
-/*   Updated: 2024/12/17 21:42:27 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/01/09 18:45:44 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "../libft/libft.h"
+# include <signal.h>
+# include <errno.h>
+# include <strings.h>
+
 
 #endif
