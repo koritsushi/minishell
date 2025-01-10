8b8760147c8af46943dc8adea3a6d895fe252114
Kodokushi <lyuan.2581@gmail.com>
2025-01-10 22:26:06 +0800

    add comments on what character to scan and tokenize in lexical.c

--- srcs/lexical.c
+++ srcs/lexical.c
@@ -6,8 +6,20 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 16:29:17 by mliyuan           #+#    #+#             */
-/*   Updated: 2025/01/07 16:29:18 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/01/09 22:45:02 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
+
+/*
+	tokenize all of these characters
+	' ' ' single quote
+	' " ' double quote
+	' < ' redirect input
+	' > ' redirect output
+	" << " delimiter
+	" >> " redirect output append mode
+	' | ' pipe
+	' $ ' environment variables 
+*/
