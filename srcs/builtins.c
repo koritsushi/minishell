3414754c36a41c42d64d3dfa86312d40a4093b04
Kodokushi <lyuan.2581@gmail.com>
2025-02-17 09:09:35 +0800

    done pwd, echo function

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -6,20 +6,39 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 14:55:34 by mliyuan           #+#    #+#             */
-/*   Updated: 2025/02/14 16:26:37 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/02/15 23:07:49 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/builtins.h"
 
-//pwd
-void    getcurrdp()
+/*
+pwd
+https://manual.cs50.io/3/getcwd
+https://sites.uclouvain.be/SystInfo/manpages/man3/getcwd.3posix.html
+https://stackoverflow.com/questions/9449241/where-is-path-max-defined-in-linux
+https://insanecoding.blogspot.com/2007/11/pathmax-simply-isnt.html
+https://pubs.opengroup.org/onlinepubs/7908799/xsh/getcwd.html
+*/
+void    getpwd()
 {
-    
+	char	buffer[PATH_MAX];
+	char	*ret;
+
+	ret = getcwd(buffer, PATH_MAX);
+    if (ret == NULL)
+	{
+		printf("get working directory fail\n", 1);
+		if (errno == ERANGE || errno == ENAMETOOLONG)
+			printf("Path exceeds max buffer length, error code:%s\n", sterror(errno));
+		else if (errno == ENOMEM)
+			printf("could not allocate memory for path, error code:%s\n", sterror(errno));
+	}
+	ft_putstr_fd(buffer, 1);
 }
 
 //cd
-void    changepath()
+void    chgwd()
 {
 
 }
@@ -58,12 +77,20 @@ void    echo(int argc, char **args)
         ft_putstr_fd("\n", 1);
 }
 
-int main (int arc, char **argv)
-{
-    echo(arc, argv);
-    return (0);
-}
+// int main (int arc, char **argv)
+// {
+//     echo(arc, argv);
+//     return (0);
+// }
 
 //export 
+void	export()
+{
+
+}
 
 //unset
+void	unset()
+{
+	
+}
