628c9075bfe0da3e0677290d95d69612a32bafec
Kodokushi <lyuan.2581@gmail.com>
2025-02-18 22:01:25 +0800

    created env.c env.h file for environment function

---
+++ srcs/env.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 15:47:13 by mliyuan           #+#    #+#             */
+/*   Updated: 2025/02/18 15:47:13 by mliyuan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/env.h"
+
+/*	env
+	used doubly linked-list to store env
+	copy env from bash shell then stored a copy in minishell
+	print out env from minishell 
+	if update env only minishell own copy of env
+*/
+void	env()
+{
+
+}
+
+void	getenv()
+{
+	
+}
+
+/*	export 
+	add variable to env
+
+*/
+void	export()
+{
+
+}
+
+/*	unset
+	remove a specific keyword from env
+*/
+void	unset()
+{
+	
+}
