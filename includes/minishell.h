11d69977172e229f1ccfd6caa208c13680dd83af
Kodokushi <lyuan.2581@gmail.com>
2025-02-18 22:01:51 +0800

    update header include

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,18 +6,20 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:20:44 by mliyuan           #+#    #+#             */
-/*   Updated: 2025/01/16 15:58:18 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/02/18 19:06:56 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-# include "../libft/libft.h"
-# include "signals.h"
-# include <errno.h>
-# include <strings.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-
+# include <errno.h>
+# include <strings.h>
+# include "../libft/libft.h"
+# include "execution.h"
+# include "builtins.h"
+# include "env.h"
+# include "signals.h"
 
 #endif
