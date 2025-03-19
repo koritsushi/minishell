c32e0cd8929128b825a42db0b8ade7972278d24c
Kodokushi <lyuan.2581@gmail.com>
2025-03-20 00:04:05 +0800

    update execution.c comment

--- srcs/execution.c
+++ srcs/execution.c
@@ -15,9 +15,14 @@
 /*
 int access() function to check in fork child of execution
 instead of checking and parsing before execution
+
 execve() can run executable of compile programs of builtins
+
 so question here is that do we run builins within minishell
 or execve it like cmds but recognize it as local lib
+
+ans: fork out a child process just for builtin function for 
+them to be able redirec in/out for shell commands function
 int	execution()
 {
 	
