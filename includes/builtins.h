42531b953e6dc9f063ece2507f6b35e08a9f4c86
Kodokushi <lyuan.2581@gmail.com>
2025-03-20 00:02:42 +0800

    added builtins function prototype in builins.h

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 14:55:59 by mliyuan           #+#    #+#             */
-/*   Updated: 2025/02/21 17:35:50 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/03/19 19:49:54 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 # define PATHMX 4096
 # include "minishell.h"
 
+char	*getpwd(void);
+void	printpath(void);
+void	chgwd(char *dir);
+void	echo(int argc, char **args);
 
 #endif
