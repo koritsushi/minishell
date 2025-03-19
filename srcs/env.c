a4f49c254f61226501c5409975af5ca01c1c9a03
Kodokushi <lyuan.2581@gmail.com>
2025-03-20 00:03:46 +0800

    writing pseudo code in the function to outline the function requirement

--- srcs/env.c
+++ srcs/env.c
@@ -16,25 +16,42 @@
 	used doubly linked-list to store env
 	copy env from bash shell then stored a copy in minishell
 */
-void env_init(t_list *lst, char **env)
+void	env_init(t_list lst, char **env)
 {
-
+	char	*str;
+	if (env == NULL)
+		return ;
+	lst->env = env;
 }
 
 /*
 	if update env only minishell own copy of env
 */
-void	env(t_list *lst)
+void	env_update(t_list **lst, t_list *lc_env)
 {
-	
+	t_list new_lst;
+
+	new_lst = ft_lstnew((void) lc_env)
+	if (new_lst == NULL)
+		return ;
+	lst->next = ft_lstadd_back(lst, new_lst);
+
 }
 
 /*
 	print out env from minishell 
 */
-void	print_env()
+void	env_print(t_list **lst)
 {
-	
+	int	i;
+
+	while (lst->next != NULL)
+	{
+		i = 0;
+		while (lst->env[i] != NULL)
+			printf("%s\n", env[i++]);
+		lst = lst->next;
+	}
 }
 
 /*	export 
@@ -47,7 +64,7 @@ void	export()
 }
 
 /*	unset
-	remove a specific keyword from env
+	remove a specific variale to display on env and export
 */
 void	unset()
 {
