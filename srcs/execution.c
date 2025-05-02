cb4632693263880f09354097b380ae580b8a88a7
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-02 18:19:31 +0800

    migrating pipex code to minishell

--- srcs/execution.c
+++ srcs/execution.c
@@ -23,8 +23,43 @@ or execve it like cmds but recognize it as local lib
 
 ans: fork out a child process just for builtin function for 
 them to be able redirec in/out for shell commands function
-int	execution()
-{
-	
-}
 */
+int	execution(t_ms *data, char *cmd_paths, char **cmd_args, char **envp)
+{
+	if (execve(cmd_paths, cmd_args, envp) == -1)
+	{
+		write(2, "./pipex: execve() error!\n", 26);
+		exit(errno);
+	}
+}
+
+static void	ft_process(t_ms *data, char **envp)
+{
+	pid_t	pid;
+	int		p_status;
+	int		i;
+
+	p_status = 0;
+	i = 0;
+	while (i < data->exec.cmd_count)
+	{
+		pid = fork();
+		if (pid == -1)
+			printf("\033[34mminishell: fork() error!\033[0m\n");
+		if (pid == 0)
+		{
+			ft_child_process(data, i);
+			ft_execute(data, data->exec.cmd_paths[i], data->exec.cmd_args[i], envp);
+		}
+		else
+		{
+			ft_parent_process(data, i);
+		}
+		i++;
+	}
+	while (wait(&p_status) >= 0)
+	{
+		if (WIFEXITED(p_status))
+			data->exec.exit_code = WEXITSTATUS(p_status);
+	}
+}
