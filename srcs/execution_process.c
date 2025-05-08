5c3792a30def8407e678cbd86549ce733c41ce3a
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-08 19:16:54 +0800

    restructuring and factor how the process redirect pipes, infile, outfile but not stdin and stdout yet

--- srcs/execution_process.c
+++ srcs/execution_process.c
@@ -6,60 +6,62 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 18:13:55 by mliyuan           #+#    #+#             */
-/*   Updated: 2025/05/06 16:35:16 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/05/08 18:52:47 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../includes/execution.h"
 
-int	execution(t_ms *data, char *cmd_paths, char **cmd_args, char **envp)
+int	execution(t_ms *data, char *cmd, char **cmd_args, char **envp)
 {
-	if (execve(cmd_paths, cmd_args, envp) == -1)
+	char *cmd_path;
+
+	if (is_built_in(cmd))
+	{
+		if (execute_built_in(data, cmd_args))
+			exit(0); //finish builtin function and free all struct in childProcess
+		else
+			exit(1); //exit and free all struct in childProcess
+	}
+	cmd_path = ft_cmdpath(cmd_args, data->exec.path);
+	if (cmd_path == NULL)
+		exit(1); //exit and free all struct in childProcess
+	if (execve(cmd_path, cmd_args, envp) == -1)
 	{
 		printf("./pipex: execve() error!\n");
-		exit(1);
+		exit(1); //exit and free all struct in childProcess
 	}
 }
 
-void	close_pipe(t_exec *data, int index)
+void	close_pipe(t_ms *data, int index)
 {
 	index = index + 1;
-	while (index < data->cmd_count - 1)
+	while (index < data->exec.cmd_count - 1)
 	{
-		close(data->pipes[index][READ]);
-		close(data->pipes[index][WRITE]);
+		close(data->exec.pipes[index][READ]);
+		close(data->exec.pipes[index][WRITE]);
 		index++;
 	}
 }
 
-void	ft_parent_process(t_exec *data, int index)
+void	ft_parent_process(t_ms *data, int index)
 {
 	if (index == 0)
-	{
-		close(data->pipes[index][WRITE]);
-		close(data->infile_fd);
-	}
-	else if (index == data->cmd_count - 1)
-	{
-		close(data->pipes[index - 1][WRITE]);
-		close(data->pipes[index - 1][READ]);
-		close(data->outfile_fd);
-	}
+		fp_process(data, index);
+	else if (index == data->exec.cmd_count - 1)
+		lp_process(data, index);
 	else
-	{
-		close(data->pipes[index - 1][READ]);
-		close(data->pipes[index][WRITE]);
-	}
+		mp_process(data, index);
 }
 
-void	ft_child_process(t_exec *exec, int index)
+void	ft_child_process(t_ms *data, int index, char **envp)
 {
 	if (index == 0)
-		f_process(exec, index);
-	else if (index == exec->cmd_count - 1)
-		l_process(exec, index);
+		f_process(data, index);
+	else if (index == data->exec.cmd_count - 1)
+		l_process(data, index);
 	else
-		m_process(exec, index);
-	ft_execute(exec, exec->cmd_paths[index], exec->cmd_args[index], exec->envp);
+		m_process(data, index);
+	ft_execute(data, data->exec.cmd[index], data->exec.cmd_args[index], envp);
 }
