cac775202adf3ca29fbf17be53a58bd48b875428
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-09 19:07:52 +0800

    add exit status code for sigint ctrl-c

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsim <hsim@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 16:12:37 by mliyuan           #+#    #+#             */
-/*   Updated: 2025/05/02 16:12:16 by hsim             ###   ########.fr       */
+/*   Updated: 2025/05/09 13:56:51 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
+
+int	g_signal = 0;
 
 int	ft_isspace(char *str)
 {
@@ -43,10 +45,15 @@ int	main(int argc, char **argv, char **env)
 	while (1)
 	{
 		text = readline("\033[34mminishell ˚𓆝 ⋆｡𓆟 ⋆｡𓆞˚ 𓇼  > \033[0m");
+		if (g_signal == 130)
+		{
+			data.exec.exit_code = 130;
+			g_signal = 0;
+		}
 		if (text == NULL)
 		{
 			ft_putstr_fd("\e[0;31mlogout\e[0;0m\n", 1);
-			exit(-1);
+			exit(1);
 		}
 		if (*text)
 			add_history(text);
