e9ad70084f5403cabc122866abc6cf006c9ae175
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-14 17:02:46 +0800

    comment token_utils_count.c int ount_infile function with /**/ multi line comment, comment out all int x variable in ft_split_shell in token_utils_split.c

--- srcs/token_utils_count.c
+++ srcs/token_utils_count.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token_utils_count.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsim <hsim@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 14:05:05 by hsim              #+#    #+#             */
-/*   Updated: 2025/05/13 08:01:05 by hsim             ###   ########.fr       */
+/*   Updated: 2025/05/14 17:00:59 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,22 +88,24 @@ static int	count_infile(char **res)//, char **infile)
 	}
 	return (count);
 }
-// static int	count_infile(char **res, char **infile)
-// {
-// 	int		count;
-// 	char	**in_fin;
+/*
+static int	count_infile(char **res, char **infile)
+{
+	int		count;
+	char	**in_fin;
 
-// 	count = 0;
-// 	in_fin = ft_split_shell(infile[0], ">");
-// 	if (res[0][0] == '<' && !infile[1] && \
-// 		has_more_str(in_fin[0], " \t\n\v\f\r"))
-// 		count++;
-// 	else if (res[0][0] != '<' && infile[1])
-// 		count++;
+	count = 0;
+	in_fin = ft_split_shell(infile[0], ">");
+	if (res[0][0] == '<' && !infile[1] && \
+		has_more_str(in_fin[0], " \t\n\v\f\r"))
+		count++;
+	else if (res[0][0] != '<' && infile[1])
+		count++;
 
-// 	free_chr_ptr((void **)in_fin);
-// 	return (count);
-// }
+	free_chr_ptr((void **)in_fin);
+	return (count);
+}
+*/
 
 /* counts number of chars in cmd_tail for malloc use */
 int	count_cmd_tail_chr(char **outfile)
--- srcs/token_utils_split.c
+++ srcs/token_utils_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token_utils_split.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsim <hsim@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 08:10:46 by hsim              #+#    #+#             */
-/*   Updated: 2025/05/14 16:29:16 by hsim             ###   ########.fr       */
+/*   Updated: 2025/05/14 17:01:09 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,7 +123,7 @@ char	**ft_split_shell(char *str, char *set)
 {
 	char	**res;
 	int		i;
-	int		x;
+	//int		x;
 	int		count;
 
 	if (!str || !set)
@@ -135,12 +135,12 @@ char	**ft_split_shell(char *str, char *set)
 	// /*debug*/printf("str_count:%d\n", str_count);
 	// res = (char **)malloc(sizeof(char *) * (str_count + 1));
 	res = (char **)malloc(sizeof(char *) * (count_str(str, set) + 1));
-	x = 0;
+	//x = 0;
 	// /*debug*/printf("\033[102mcount_str= %d+1\033[0m\n", count_str(str, set));
 	while (str[0] && count_str(str, set))
 	{
 		// /*debug*/printf("split_enter:%s\n", str);
-		x = 0;
+		//x = 0;
 		count = count_chr(str, set);//, &f);
 		if (!malloc_chr_ptr(&res[i], count + 1))
 		{
