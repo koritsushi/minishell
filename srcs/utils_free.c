154d43ffced533701089eadfa2d4ff0e6a5e12dd
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-20 19:48:32 +0800

    move free function to utils_free2.c

--- srcs/utils_free.c
+++ srcs/utils_free.c
@@ -6,7 +6,7 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 17:58:29 by hsim              #+#    #+#             */
-/*   Updated: 2025/05/16 09:21:38 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/05/20 16:36:39 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,31 +93,4 @@ void	free_all(t_token *lst)
 		return ;
 	free_chr_ptr((void **)lst->data);
 	free(lst->datatype);
-}
-
-void	free_env(t_env *env)
-{
-	if (env && env->content)
-		ft_lstclear_sh(&env, free);
-}
-
-void	free_exec(t_exec *exec)
-{
-	if (exec->cmd_args)
-		free_3d_ptr((void ***)exec->cmd_args);
-	if (exec->cmd)
-		free_chr_ptr((void **)exec->cmd);
-	if (exec->path)
-		free_chr_ptr((void **)exec->path);
-}
-
-void	ms_free_all(t_ms *data, int exit_code)
-{
-	free_env(data->env_var);
-	free_exec(&data->exec);
-	if (data->lst.data)
-		free_chr_ptr((void **)data->lst.data);
-	if (data->lst.datatype)
-		free(data->lst.datatype);
-	exit(exit_code);
 }
