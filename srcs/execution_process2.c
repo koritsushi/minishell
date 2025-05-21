272efa61f22a9020515fb5c6db64627228cf40ca
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-21 15:05:30 +0800

    move execution.c function to this file to conform norm stand

---
+++ srcs/execution_process2.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution_process2.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 11:03:43 by mliyuan           #+#    #+#             */
+/*   Updated: 2025/05/21 11:29:47 by mliyuan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/execution.h"
+
+int	ft_isspace(char c)
+{
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (0);
+	return (1);
+}
+
+int	ft_isempty(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && ft_isspace(str[i]) == 1)
+		i++;
+	if (str[i] == '\0')
+		return (1);
+	return (0);
+}
+
+int	lst_cmd_count(t_token *lst)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = 0;
+	while (lst->data[i] != NULL)
+	{
+		if (lst->datatype[i++] == WORD)
+			size++;
+	}
+	return (size);
+}
+
+int	lst_pipe_count(t_token *lst)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = 0;
+	while (lst->data[i] != NULL)
+	{
+		if (lst->datatype[i] == PIPE)
+			size++;
+		i++;
+	}
+	return (size);
+}
