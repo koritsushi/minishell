2dcf58c27387c229391f5b3b0601f8230bda846e
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-21 14:54:38 +0800

    change sigint_handler to signal_handler and added change set_signal_action param from void to int and comment old signal blocking function

--- includes/signals.h
+++ includes/signals.h
@@ -15,9 +15,9 @@
 # include "minishell.h"
 # include <signal.h>
 
-void	set_signal_action(void);
-void	block_signal(int signal);
-void	unblock_signal(int signal);
-void	sigint_handler(int signal);
+//void	block_signal(int signal);
+//void	unblock_signal(int signal);
+void	signal_handler(int signal);
+void	set_signal_action(int code);
 
 #endif
