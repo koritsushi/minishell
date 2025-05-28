a9854060b0cdc2799abeacf6ea13b8e725f20314
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-28 13:44:39 +0800

    remove unused and undeclare function ft_cmdpath_error()

--- includes/execution.h
+++ includes/execution.h
@@ -37,7 +37,6 @@ char	**ft_get_path(char **envp);
 char	**ft_format_path(char **path, char *format);
 char	***ft_split_cmd(t_exec *exec, char **argv);
 char	*ft_cmdpath(char **cmd_args, char **path);
-void	ft_cmdpath_error(t_ms *data, char *cmd, char **path);
 
 //execution_cmd_utils.c
 char	**ft_envp(t_env **lst);
