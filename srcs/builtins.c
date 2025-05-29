2ea8a6da022860f29ee04c231d8ce573bcc2f82c
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-29 16:16:21 +0800

    change if statement checking option -n from ft_strncmp to ft_strcmp for ft_echo

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -6,7 +6,7 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 14:55:34 by mliyuan           #+#    #+#             */
-/*   Updated: 2025/05/21 11:45:21 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/05/29 16:01:08 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,7 +126,7 @@ int	ft_echo(char **args)
 	int	nl;
 
 	nl = 0;
-	if (args[1] && ft_strncmp(args[1], "-n", 2) == 0)
+	if (args[1] && ft_strcmp(args[1], "-n") == 0)
 		nl = 1;
 	i = 1 + nl;
 	while (args[i] != NULL)
