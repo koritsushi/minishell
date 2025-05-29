6b978eb937d1b3c02f8a0611d38a4f3d702cdb29
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-29 19:55:23 +0800

    add ft_isempty2 to header

--- includes/execution.h
+++ includes/execution.h
@@ -31,6 +31,7 @@ void	ft_process(t_ms *data);
 int		lst_cmd_count(t_token *lst);
 int		lst_pipe_count(t_token *lst);
 int		ft_isempty(char *str);
+int		ft_isempty2(char *str);
 
 //execution_cmd.c functions
 char	**ft_get_path(char **envp);
