cd119d77ba41ab14658af9431113e8cdf4066cfe
Kodokushi <lyuan.2581@gmail.com>
2025-05-30 12:04:09 +0800

    change name

---
+++ srcs/execution_process_utils.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution_process2.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 11:03:43 by mliyuan           #+#    #+#             */
+/*   Updated: 2025/05/29 19:40:30 by mliyuan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/execution.h"
+
+int	ft_isspace(char c)
+{
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
+}
+
+int	ft_isempty(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL)
+		return (1);
+	while (str[i] != '\0')
+	{
+		if (ft_isspace(str[i]))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int	ft_isempty2(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL)
+		return (1);
+	while (str[i] != '\0')
+	{
+		if (ft_isspace(str[i]))
+			i++;
+		else
+			return (0);
+	}
+	if (i <= 1)
+		return (0);
+	return (1);
+}
+
+int	lst_cmd_count(t_token *lst)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = 0;
+	while (lst->data[i] != NULL)
+	{
+		if (lst->datatype[i++] == WORD)
+			size++;
+	}
+	return (size);
+}
+
+int	lst_pipe_count(t_token *lst)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = 0;
+	while (lst->data[i] != NULL)
+	{
+		if (lst->datatype[i] == PIPE)
+			size++;
+		i++;
+	}
+	return (size);
+}
