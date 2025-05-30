087a4f4648b5528a504b44d3e3d0e5d0376bd86d
Mah Liyuan <lyuan.2581@gmail.com>
2025-05-30 17:29:04 +0800

    remove commented function

--- includes/env.h
+++ includes/env.h
@@ -27,14 +27,12 @@ t_env	*ft_lstnew_env(char *name, char *content, int export_id);
 void	ft_lst_remove_if(t_env **lst, char *target, int (*func)());
 
 int		ft_array_len(char **arr);
-void	msh_init(t_ms *data, char** env);
+void	msh_init(t_ms *data, char **env);
 void	env_init(t_env **env_var, char **env);
 void	exec_init(t_exec *exec);
-// void	export(t_env **env_var, t_env *lenv);
-int 	unset(t_env **lst, char *str);
-// void	unset(t_env **env_var, t_env *lenv);
-int 	env_print(t_env **lst);
-int 	export_print(t_env **lst);
+int		unset(t_env **lst, char *str);
+int		env_print(t_env **lst);
+int		export_print(t_env **lst);
 void	split_env(char **env, char **var);
 int		ft_array_len(char **str);
 
