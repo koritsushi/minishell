64134922e023a684a3a7d42b33351f460ef4421b
Kodokushi <lyuan.2581@gmail.com>
2025-05-30 12:02:06 +0800

    move child process builtin function call to ft_process_built_in and change print errstr from putstr_fd to perror()

--- srcs/execution_process.c
+++ srcs/execution_process.c
@@ -6,16 +6,14 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 18:13:55 by mliyuan           #+#    #+#             */
-/*   Updated: 2025/05/29 19:10:41 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/05/30 10:58:35 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/execution.h"
 
-void	ft_execution(t_ms *data, char *cmd, char **cmd_args)
+static void	ft_process_built_in(t_ms *data, char *cmd, char **cmd_args)
 {
-	char	*cmd_path;
-
 	if (is_built_in(cmd))
 	{
 		if (execute_built_in(data, cmd_args))
@@ -23,22 +21,25 @@ void	ft_execution(t_ms *data, char *cmd, char **cmd_args)
 		else
 			ms_free_all(data, -1, 1);
 	}
+}
+
+void	ft_execution(t_ms *data, char *cmd, char **cmd_args)
+{
+	char	*cmd_path;
+	char	*err;
+
+	ft_process_built_in(data, cmd, cmd_args);
 	cmd_path = ft_cmdpath(cmd_args, data->exec.path);
 	if (cmd_path == NULL)
 	{
-		char	*str1 = ft_strjoin("./minishell: ", cmd_args[0]);
-		char	*str2 = ft_strjoin(": ", strerror(errno));
-		char	*nl = ft_strjoin(str2, "\n");
-		char	*error = ft_strjoin(str1, nl);
-		ft_putstr_fd(error, 2);
-		free(error);
-		char *testShits = malloc(sizeof(char) * 329);
-		(void)testShits;
+		err = ft_strjoin("-minishell: ", cmd_args[0]);
+		perror(err);
+		free(err);
 		ms_free_all(data, -1, 127);
 	}
 	if (execve(cmd_path, cmd_args, data->exec.envp) == -1)
 	{
-		ft_putstr_fd("./minishell: execve() error!\n", 2);
+		perror("-minishell: execve() error!");
 		free(cmd_path);
 		ms_free_all(data, -1, -1);
 	}
